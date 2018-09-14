64ccf5f87927d412f0938cf238e9ab07ddc10d1b
lornamicheller <37231071+lornamicheller@users.noreply.github.com>
2018-09-14 13:43:49 -0400

    last commit

--- js/ladderllc_script.js
+++ js/ladderllc_script.js
@@ -1,5 +1,11 @@
 /* global $ */
 
-$(document).ready(function(){
+$(document).ready(function() {
   $('.slider').slick();
-});
+})
+
+$(document).ready(function() {
+  $('.menu-toggle').click(function() {
+    $('nav').toggleClass('active');
+  })
+})
